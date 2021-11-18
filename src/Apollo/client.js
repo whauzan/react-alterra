d666f70f9846fcc517f8a1f97a56e5085bdfc41b
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2021-11-19 02:03:27 +0700

    fix: websocket

--- src/Apollo/client.js
+++ src/Apollo/client.js
@@ -1,10 +1,42 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, InMemoryCache, split, HttpLink } from '@apollo/client'
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
+
+const wsLink = new WebSocketLink({
+	uri: "wss://wahyuhr-query-apollo.hasura.app/v1/graphql",
+	options: {
+		reconnect: true,
+		connectionParams: {
+			headers: {
+				"x-hasura-admin-secret":
+					"zHHZrC7K5vwiFyiJez7emtWAU8ki5bi0EJ5tP7hJFXQ6U1NN9e2tGV8cFMYWaVnq",
+			},
+		},
+	},
+});
+
+const httpLink = new HttpLink({
+	uri: "https://wahyuhr-query-apollo.hasura.app/v1/graphql",
+	headers: {
+		"x-hasura-admin-secret":
+			"zHHZrC7K5vwiFyiJez7emtWAU8ki5bi0EJ5tP7hJFXQ6U1NN9e2tGV8cFMYWaVnq",
+	},
+});
+
+const splitLink = split(
+	({ query }) => {
+		const definition = getMainDefinition(query);
+		return (
+			definition.kind === "OperationDefinition" &&
+			definition.operation === "subscription"
+		);
+	},
+	wsLink,
+	httpLink
+);
 
 const client = new ApolloClient({
-    uri: 'https://wahyuhr-query-apollo.hasura.app/v1/graphql',
-    headers: {
-    'x-hasura-admin-secret': 'zHHZrC7K5vwiFyiJez7emtWAU8ki5bi0EJ5tP7hJFXQ6U1NN9e2tGV8cFMYWaVnq'
-    },
+    link: splitLink,
     cache: new InMemoryCache(),
 })
 
